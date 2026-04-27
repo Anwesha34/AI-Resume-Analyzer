98dd0e58e94907658b2dfde63fe4cc17438eeae4
Anwesha Das <anweshad200@gmail.com>
2026-04-27 15:42:07 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,9 +1,11 @@
 import os
+import streamlit as st
 from langchain_groq import ChatGroq
 from langchain_core.prompts import PromptTemplate
 
+
 # API KEY
-os.environ["GROQ_API_KEY"] = "YOUR_API_KEY"
+os.environ["GROQ_API_KEY"] = st.secrets["GROQ_API_KEY"]
 
 # LLM Model
 llm = ChatGroq(
